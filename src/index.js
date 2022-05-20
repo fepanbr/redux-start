c33dd5c027cc8e7f9279ed73cba40225f1feb4f5
fepanbr <fepanbr@gmail.com>
2022-05-20 22:59:04 +0900

    feat(reducer): combine 리듀서

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
-import {addTodo} from "./redux/actions"
+import {addTodo, completeTodo, showComplete} from "./redux/actions"
 
 // 스토어의 변경사항을 구독
 store.subscribe(() => {
@@ -12,6 +12,9 @@ store.subscribe(() => {
 
 })
 
+store.dispatch(addTodo('할일'));
+store.dispatch(completeTodo(0));
+store.dispatch(showComplete())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,7 @@
 export const ADD_TODO = 'ADD_TODO';
-export const DONE_TODO = 'DONE_TODO';
+export const COMPLETE_TODO = 'COMPLETE_TODO';
+export const SHOW_ALL = 'SHOW_ALL';
+export const SHOW_COMPLETE = 'SHOW_COMPLETE';
 
 // 액션 생성자
 export function addTodo(text) {
@@ -9,3 +11,21 @@ export function addTodo(text) {
   };
 }
 
+export function completeTodo(index) {
+  return {
+    type: COMPLETE_TODO,
+    index
+  }
+}
+
+export function showAll() {
+  return {
+    type: SHOW_ALL,
+  }
+}
+
+export function showComplete() {
+  return {
+    type: SHOW_COMPLETE,
+  }
+}
